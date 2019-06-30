7c5a45c92075b03021c736b99a5a1413500893cf
catp0wer <catp0wer@mail.ru>
2019-06-30 23:37:44 +0300

    simpliest example of cucumber with Feature file, Test runner and Step definitions

---
+++ src/test/java/testRunners/TestRunner.java
@@ -0,0 +1,17 @@
+package testRunners;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(features="src/test/resources/Features", glue= "StepDefinitions")
+
+//NOTE: Maven test autodiscovery will only look for classes that follow one of these patterns:
+//Test* or *Test, and it is case sensitive. So `testRunner` wouldn't work, and you'll have no hint
+//about why that is the case
+public class TestRunner {
+
+}
